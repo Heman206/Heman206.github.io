171b3b88b863e208d965c727d883e0fd662fb941
Janna Fasheh <janna.fasheh@warriorlife.net>
2018-05-15 10:40:54 -0700

    uploading again

---
+++ _posts/2018-05-15-fourth-post.markdown
@@ -0,0 +1,13 @@
+---
+layout: post
+title:  "Fourth Post"
+date:   2018-05-15 10:38:49 -0700
+categories: jekyll update
+author: Heman206
+---
+
+When I was in junior high, I took an intro to programming class. I learned HTML, CSS, and javascript. Here's the site I made.
+
+[project site][url]
+
+[url]: /website/index.html
---
+++ website/calculator/calculator.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+// Change Display
+function d(val) {
+		"use strict";
+		document.getElementById("d").value = val;
+}
+
+// Type numbers and operators
+function v(val) {
+		"use strict"; 
+		document.getElementById("d").value += val;
+}
+
+//Evaluate the equation
+function e() {
+		"use strict";
+		try {
+				d(eval(document.getElementById("d").value));
+		}
+		catch(err) {
+				d("Error");
+		}
+}
